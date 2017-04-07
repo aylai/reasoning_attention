b86e853c09a282338ea5b733cb87a811cf24af5a
aylai2 <aylai2@illinois.edu>
2017-04-07 10:58:33 +0200

    add grid search

--- run_grid_search.sh
+++ run_grid_search.sh
@@ -2,5 +2,5 @@
 
 gpu=$1
 for i in `seq 0 20`; do
-    CUDA_VISIBLE_DEVICES=$gpu python3 ./snli_reasoning_attention.py --grid_id $i --model_type word_by_word --model_name gridsearch_att_$i --train --data_dir mpe
+    CUDA_VISIBLE_DEVICES=$gpu python3 ./snli_reasoning_attention_gridsearch.py --grid_id $i --model_type word_by_word --model_name gridsearch_att_$i --train --data_dir mpe
 done
