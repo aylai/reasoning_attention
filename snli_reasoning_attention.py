f8cb530581af5651c71e83688725586997afc9f6
aylai2 <aylai2@illinois.edu>
2017-04-09 14:03:05 -0500

    fix eval test?

--- snli_reasoning_attention.py
+++ snli_reasoning_attention.py
@@ -492,7 +492,11 @@ def main(params, load_model=None):
         print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
         print("  test accuracy:\t\t{:.2f} %".format(
             test_acc / test_batches * 100))
-        print("  final test acc: {:.2f} %".format(np.sum(predictions == targets)))
+        print(type(predictions))
+        print(predictions[1:10])
+        print(type(targets))
+        print(targets[1:10])
+        print("  final test acc: {:.2f} %".format(np.sum(np.asarray(predictions) == np.asarray(targets))))
         print(precision_recall(predictions, targets))
 
 
